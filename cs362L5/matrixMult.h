0046a17b0afd2afd5b2d3fa33345d2f31e90955a
rafaeldiaz619 <rafael.diaz619@myci.csuci.edu>
2025-02-26 18:43:34 -0800

    LAB5
    this is for LAB5

---
+++ cs362L5/matrixMult.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define oops(errmsg, err) { perror(errmsg); exit(err); }
+
+// struct for passing indices to the threads
+typedef struct matrixCell
+{
+   int i;
+   int j;
+   int k;
+   int** a;
+   int** b;
+   int** c;
+} MATRIX_CELL;
+
+void* matrixThread(void*);
+void allocateAndLoadMatrices(int***, int***, int***, int*, int*, int*);
+int** allocMat(int *, int*);
+void loadMatrix(int***, int, int);
+void freeMatrix(int**, int);
+pthread_t** alloc_tids(int, int);
+void free_thread_ids(pthread_t**, int);
+pthread_t** multiply(int**, int**, int**, int, int, int);
+void join(pthread_t**, int, int);
+
+void displayMatrix(int **, int, int);
